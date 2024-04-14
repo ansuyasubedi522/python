3f98aa043a624eab305eb401388ff3b43bf8f295
ansuyasubedi522 <166967085+ansuyasubedi522@users.noreply.github.com>
2024-04-13 22:42:46 -0400

    wrote about complex data type

--- datatype.py
+++ datatype.py
@@ -1,9 +1,11 @@
 '''
 This is my first python program 
 k is a variable. it is asking user to input integer value
-int is one of the data types it takes the number with out the decimal (whole number)
-float is a data type in the pyton which takes the decimal number
-string  is a data type string are used to repesent data it can be used with single or dubble quots
+int: is one of the data types it takes the number with out the decimal (whole number)
+float: is a data type in the pyton which takes the decimal number
+string:  is a data type string are used to repesent data it can be used with single or dubble quotes
+complex: it can be mix of numner and words 1k 2nm2
+
 
 '''
 
