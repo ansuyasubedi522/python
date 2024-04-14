d0e3cdec84a47de6d74b4bd08c57016661f4fdb2
ansuyasubedi522 <166967085+ansuyasubedi522@users.noreply.github.com>
2024-04-13 22:32:49 -0400

    Create datatype.py
    i am collecting python data types

---
+++ datatype.py
@@ -0,0 +1,14 @@
+'''
+This is my first python program 
+k is a variable. it is asking user to input integer value
+int is one of the data types it takes the number with out the decimal
+float is a data type in the pyton which takes the decimal number
+string  is a data type string are used to repesent data it can be used with single or dubble quots
+
+'''
+
+#how=input("please enter your name")
+#print (how)
+
+k=int(input("please enter your number"))
+print (k)
